d426ac39e9afa672b798e8e17108ba0c0600dbe7
kristinka-skl <kristinka.skl@gmail.com>
2025-07-27 22:50:56 +0200

    ready

---
+++ src/js/2-form.js
@@ -0,0 +1,43 @@
+const formData = {
+    email: '',
+    message: '',
+}
+const localStorageKey = 'feedback-form-state';
+
+const formElem = document.querySelector('.feedback-form');
+formElem.addEventListener('input', handleFormElem);
+function handleFormElem(evt) {           
+    formData.email = evt.currentTarget.elements.email.value.trim();
+    formData.message = evt.currentTarget.elements.message.value.trim();
+    localStorage.setItem(localStorageKey, JSON.stringify(formData));    
+}
+const emailArea = formElem.elements.email;
+const messageArea = formElem.elements.message;
+const userData = localStorage.getItem(localStorageKey);
+if (userData !== null) {
+    try {    
+        const parsedUserData = JSON.parse(userData);
+        emailArea.value = parsedUserData.email;
+        messageArea.value = parsedUserData.message;
+    } catch (error) {
+        console.log(error);    
+    }
+} else {
+    emailArea.value = '';
+    messageArea.value = '';
+}
+
+formElem.addEventListener("submit", (evt) => {
+    if (formData.email !== '' && formData.message !== '') {
+        evt.preventDefault();
+        console.log(formData);
+        localStorage.removeItem(localStorageKey);
+        formElem.reset();
+        formData = {
+            email: '',
+            message: '',
+        }
+    } else {
+        alert('Fill please all fields');
+    }   
+});
